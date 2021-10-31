e5e4580954bbdddf58e8f4512e14e028e7524aeb
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2021-10-31 13:29:47 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,26 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article_id = params[:id].to_i
+    erb :show
+  end
+
+  get 'articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    binding.pry
+    article = Article.create(title: params[:title], content: params[:content])
+    article
+    erb :show    
+  end
+
 end
